b43d71c32ed9941fd0831daa52fc11546708e988
pavani <pavanijejammagari1053@gmail.com>
2023-04-28 12:21:09 +0530

    creation of tables and database

---
+++ query2.sql
@@ -0,0 +1,31 @@
+create database Herovired;
+create table RGM(
+Name varchar(50) primary key not null,
+session varchar(40) not null,
+contact_number int,
+Address varchar(100)
+);
+INSERT INTO RGM VALUES("pavani","first",123,"atp")
+INSERT INTO RGM VALUES("sunil","second",1123335,"anatapur")
+INSERT INTO RGM VALUES("naveen","third",8979823,"kurnool")
+INSERT INTO RGM VALUES("lakshmi","fourth",5432563,"thirupathi")
+INSERT INTO RGM VALUES("devi","fifth",12746532,"kadapa")
+INSERT INTO RGM VALUES("somu","six",87665432,"vijayawada")
+INSERT INTO RGM VALUES("shekar","seven",138905312,"krishna")
+INSERT INTO RGM VALUES("likhitha","eighth",126532683,"nandyal")
+INSERT INTO RGM VALUES("deepti","ninth",123122332,"knl")
+INSERT INTO RGM VALUES("sweha","tenth",1413223,"kurnool")
+INSERT INTO RGM VALUES("vinod","first",125678833,"anatapur")
+select *from RGM;
+
+
+
+
+
+Database - HeroVired
+
+Table - RGM
+Columns - Name, College, Session, Contact Number, Address
+
+
+10 entries........
